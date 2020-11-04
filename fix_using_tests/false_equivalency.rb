b36cbd78576041a261b002681e292a63b6dfdb74
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-04 23:27:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -2,7 +2,7 @@
 require 'pry'
 
 def get_user_input
-  get.chomp
+  num = 2
 end
 def prompt_user
   puts "What would you like to do?"
@@ -10,7 +10,7 @@ def prompt_user
   puts "2.) Eat a ham."
 
 end
-def selection(num = 2)
+def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
